6baa652327e2bb15af6f392243d387f51a7fe4a5
yangxl <yangxl@uyunsoft.cn>
2019-02-14 09:15:05 +0800

    add @type/three for threejs in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import { Rate } from 'antd';
-import * as React from 'react';
-import './App.scss';
-
-import logo from './logo.svg';
+import * as React from 'react'
+import './App.scss'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Rate character="skr,skr,skr"/>
+        hello world
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
