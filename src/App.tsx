ccf5983657f76596bc714ec243e92fe8be7c0a76
a876691666 <928200728@qq.com>
2019-02-18 23:30:30 +0800

    Feat: 完善ts类型，增加line

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,63 @@ import logo from './logo.svg';
 
 class App extends React.Component {
   setRef = (element: any) => {
-    const pie = new FeCharts.Charts.Pie(element, {});
+    const pie = new FeCharts.Charts.Pie(element, {
+      data: [
+        {
+          name: "苹果",
+          value: 40
+        },
+        {
+          name: "香蕉",
+          value: 30
+        },
+        {
+          name: "橘子",
+          value: 20
+        },
+        {
+          name: "葡萄",
+          value: 40
+        },
+        {
+          name: "芒果",
+          value: 20
+        }
+      ]
+    });
+  }
+  setLine = (element: any) => {
+    const line = new FeCharts.Charts.Line(element, {
+      ascending: true,
+      data: [
+        {
+          name: "苹果",
+          value: 405
+        },
+        {
+          name: "香蕉",
+          value: 30
+        },
+        {
+          name: "橘子",
+          value: 205
+        },
+        {
+          name: "葡萄",
+          value: 40
+        },
+        {
+          name: "芒果",
+          value: 20
+        }
+      ]
+    });
   }
   render() {
     return (
       <div className="App">
         <svg width={500} height={500} ref={this.setRef} />
+        <svg width={500} height={500} ref={this.setLine} />
       </div>
     )
   }
